90b621af33a74e10f3634d437973626636b6390b
ekankam <eb.kankam26@gmail.com>
2023-01-16 17:02:23 -0600

    Fix caching issue

--- src/utils/fetcher.ts
+++ src/utils/fetcher.ts
@@ -1,5 +1,5 @@
 export const fetchData = async (url: RequestInfo | URL) => {
-  const response = await fetch(url);
+  const response = await fetch(url, { cache: "no-cache" });
   const data = await response.json();
   return data;
 }
