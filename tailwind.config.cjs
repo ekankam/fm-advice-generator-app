2305071e59c6cdadf1da9c59a3737083cf36ce1c
ekankam <eb.kankam26@gmail.com>
2023-01-16 01:11:46 -0600

    Setup tailwindcss

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -4,7 +4,19 @@ module.exports = {
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    fontSize: {
+      default: '28px'
+    },
+    colors: {
+      'dark-blue-900': 'hsl(218, 23%, 16%)',
+      'dark-blue-500': 'hsl(217, 19%, 24%)',
+      'dark-blue-200': 'hsl(217, 19%, 38%)',
+      'light-cyan': 'hsl(193, 38%, 86%)',
+      'neon-green': 'hsl(150, 100%, 66%)'
+    },
+    boxShadow: {
+      default: '0px 0px 40px hsl(150, 100%, 66%)'
+    }
   },
   plugins: [],
 }
